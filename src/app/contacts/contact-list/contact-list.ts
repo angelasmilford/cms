826da4dafc7db1c95734e144c7024dc9fade05a3
Angela S. Milford <angela.milford123@gmail.com>
2026-05-20 14:57:00 -1100

    Displayed the Contact List component

--- src/app/contacts/contact-list/contact-list.ts
+++ src/app/contacts/contact-list/contact-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,4 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.css',
 })
-export class ContactList {}
+export class ContactList {
+  contacts: Contact[] = [
+    new Contact(
+      "1",
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null,
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null,
+    ),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onCreateContact() {
+    
+  }
+}
--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -1,12 +1,12 @@
 export class Contact {
-    public id: number;
+    public id: string;
     public name: string;
     public email: string;
     public phone: string;
     public imageUrl: string;
-    public group: Contact[];
+    public group: null | Contact[];
 
-    constructor(id: number, name: string, email: string, phone: string, imageUrl: string, group: Contact[]) {
+    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group: null | Contact[]) {
         this.id = id;
         this.name = name;
         this.email = email;
