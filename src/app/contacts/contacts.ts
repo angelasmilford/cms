8bea869e69cb010396bca09aaf860e76d68ef666
Angela S. Milford <angela.milford123@gmail.com>
2026-06-03 22:55:14 -1100

    Removed null because it was probably useless

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact.model';
   styles: ``,
 })
 export class Contacts {
-  selectedContact: Contact | null = null;
+  selectedContact: Contact;
 
   constructor() { }
 
