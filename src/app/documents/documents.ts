035879aa88d86163f63bb4232ad9bff819d76203
Angela S. Milford <angela.milford123@gmail.com>
2026-06-03 22:54:11 -1100

    Implemented the Documents component

--- src/app/documents/documents.ts
+++ src/app/documents/documents.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Document } from './document.model';
+
 @Component({
   selector: 'app-documents',
   standalone: false,
   templateUrl: './documents.html',
   styles: ``,
 })
-export class Documents {}
+export class Documents {
+  selectedDocument: Document;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
