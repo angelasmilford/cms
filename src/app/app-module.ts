e27b9a14ceda788b0f772026c6d7a7b54af27ae5
Angela S. Milford <angela.milford123@gmail.com>
2026-06-03 23:47:13 -1100

    Created a custom directive for dropdown

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -15,6 +15,7 @@ import { Messages } from './messages/messages';
 import { MessageItem } from './messages/message-item/message-item';
 import { MessageEdit } from './messages/message-edit/message-edit';
 import { MessageList } from './messages/message-list/message-list';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { MessageList } from './messages/message-list/message-list';
     MessageEdit,
     MessageList,
   ],
-  imports: [BrowserModule, Header],
+  imports: [BrowserModule, Header, DropdownDirective],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
