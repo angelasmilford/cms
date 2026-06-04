58f09f0b1a1561f63038243b131bf7527945d5da
Angela S. Milford <angela.milford123@gmail.com>
2026-06-03 16:43:00 -1100

    Displayed and added messages to the Message List component

--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Message } from '../message.model';
+
 @Component({
   selector: 'app-message-list',
   standalone: false,
   templateUrl: './message-list.html',
   styleUrl: './message-list.css',
 })
-export class MessageList {}
+export class MessageList {
+  messages: Message[] = [
+    new Message('1', 'Subject 1', 'This is a test message.', 'Name'),
+    new Message('2', 'Subject 2', 'This is another test message.', 'Name'),
+    new Message('3', 'Subject 3', 'This is yet another test message.', 'Name'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
+}
