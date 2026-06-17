ef3813f1961686feea4e5107a28a047c55bf802d
Angela S. Milford <angela.milford123@gmail.com>
2026-06-17 12:18:26 -1100

    Defined [routerLink] Directives for Each Route

--- src/app/header.ts
+++ src/app/header.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,9 +6,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
     standalone: false
 })
 export class Header {
-    @Output() selectedFeatureEvent = new EventEmitter<string>();
-
-    onSelected(selectedEvent: string) {
-        this.selectedFeatureEvent.emit(selectedEvent);
-    }
+    constructor() { }
 }
