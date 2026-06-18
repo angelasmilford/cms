21ab785f0ce80e2750f23924f90961189c8d1d3f
Angela S. Milford <angela.milford123@gmail.com>
2026-06-18 10:26:34 -1100

    Implemented the Child Routes for Contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { MessageList } from './messages/message-list/message-list';
 import { Contacts } from './contacts/contacts';
 import { DocumentEdit } from './documents/document-edit/document-edit';
 import { DocumentDetail } from './documents/document-detail/document-detail';
+import { ContactEdit } from './contacts/contact-edit/contact-edit';
+import { ContactDetail } from './contacts/contact-detail/contact-detail';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/documents', pathMatch: 'full' },
@@ -16,7 +18,11 @@ const appRoutes: Routes = [
         { path: ':id/edit', component: DocumentEdit }
     ] },
     { path: 'messages', component: MessageList },
-    { path: 'contacts', component: Contacts }
+    { path: 'contacts', component: Contacts, children: [
+        { path: 'new', component: ContactEdit },
+        { path: ':id', component: ContactDetail },
+        { path: ':id/edit', component: ContactEdit }
+    ] }
 ]
 
 @NgModule({
