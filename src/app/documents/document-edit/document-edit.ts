3d974d80a40389090cc092972eb572b90ac65c0d
Angela S. Milford <angela.milford123@gmail.com>
2026-06-30 20:46:33 -1100

    Implemented the Behavior of the Document Edit Form

--- src/app/documents/document-edit/document-edit.ts
+++ src/app/documents/document-edit/document-edit.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-edit',
@@ -9,17 +12,65 @@ import { Document } from '../document.model';
   styleUrl: './document-edit.css',
 })
 export class DocumentEdit {
-  originalDocument: Document;
-  document: Document;
+  originalDocument: Document | null = null;
+  document: Document | null = null;
   editMode: boolean = false;
 
-  constructor() { }
+  constructor(private documentService: DocumentService,
+              private router: Router,
+              private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          const id = params['id'];
+
+          if(id == null) {
+            this.editMode = false;
+            return;
+          }
+
+          this.originalDocument = this.documentService.getDocument(id);
+
+          if(this.originalDocument == null) {
+            return;
+          }
+
+          this.editMode = true;
+
+          this.document = JSON.parse(JSON.stringify(this.originalDocument));
+        }
+      );
+  }
   
   onCancel() {
-
+    this.router.navigate(['/documents']);
   }
 
   onSubmit(form: NgForm) {
+    const value = form.value;
 
+    const newDocument = new Document(
+      '',
+      value.name,
+      value.description,
+      value.url,
+    );
+
+    if(this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+
+    this.router.navigate(['/documents']);
+  }
+
+  private initForm() {
+    let documentName = '';
+    let documentDescription = '';
+    let documentUrl = '';
   }
 }
