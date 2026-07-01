7f7f8723bb373cb33839286b22530c43be888bf0
Angela S. Milford <angela.milford123@gmail.com>
2026-07-01 11:20:20 -1100

    Implemented the Behavior of the Contact Edit Form

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,4 +11,67 @@ import { Component } from '@angular/core';
   templateUrl: './contact-edit.html',
   styleUrl: './contact-edit.css',
 })
-export class ContactEdit {}
+export class ContactEdit {
+  originalContact: Contact | null = null;
+  contact: Contact | null = null;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+
+  constructor(private contactService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          const id = params['id'];
+
+          if(id == null) {
+            this.editMode = false;
+            return;
+          }
+
+          this.originalContact = this.contactService.getContact(id);
+
+          if(this.originalContact == null) {
+            return;
+          }
+
+          this.editMode = true;
+
+          this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+          if (this.contact?.group != null) {
+            this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+          }
+        }
+      );
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+
+    const newContact = new Contact(
+      '',
+      value.name,
+      value.email,
+      value.phone,
+      value.imageUrl,
+      this.groupContacts,
+    );
+
+    if (this.editMode && this.originalContact) {
+      this.contactService.updateDocument(this.originalContact, newContact);
+    } else {
+      this.contactService.addDocument(newContact);
+    }
+
+    this.router.navigate(['/contacts']);
+  }
+}
